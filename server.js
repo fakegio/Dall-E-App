7f2e858323f8621d61e48532b4135cc7f5e64339
Gio Chula <122946460+fakegio@users.noreply.github.com>
2024-05-15 20:33:19 -0400

    changed env config

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import { OpenAI } from "openai";
 import express from 'express';
 import cors from 'cors';
 
-dotenv.config({path: '../.env'});
+dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
